a0f1e426f3da202d3e8b5ade1c091fd1b95335a1
NUOYANC <nuoyanc@student.unimelb.edu.au>
2020-09-11 17:55:39 +1000

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,25 @@
+import bagel.AbstractGame;
+
+public class Game extends AbstractGame {
+
+    public Game() {
+        
+    }
+
+    
+
+    /**
+     * The entry point for the program.
+     */
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.run();
+    }
+
+    @Override
+    public void update(Input input) {
+        
+    }
+
+}
